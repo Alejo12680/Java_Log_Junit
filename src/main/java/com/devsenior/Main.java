aa4a62fc6cebb3a837e74b88e2c48584a66da98d
Alejo12680 <alejo152725@gmail.com>
2025-02-11 17:04:12 -0500

    Feat: Proyecto terminado con errores

--- src/main/java/com/devsenior/Main.java
+++ src/main/java/com/devsenior/Main.java
@@ -5,7 +5,18 @@ public class Main {
 		
 		// Instancia de inventario
 		var inventory = new Inventory();
+
+		// Agrega productos al inventario
 		inventory.addProduct(new Product("Laptop", 10, 1000d));
-		
+		inventory.addProduct(new Product("Mouse", 50, 20d));
+
+		// Vende un producto
+		inventory.sellProduct("Laptop", 5);
+		inventory.sellProduct("Mouse", 10);
+
+		// Calcula el valor total del inventario
+		var total = inventory.calculateTotalInventory();
+		System.out.printf("Valor total del inventario es: $ %.2f%n" , total);	
+
 	}
 }
